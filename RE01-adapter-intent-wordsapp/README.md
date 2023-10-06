c6d9dbbcff687f19cb02f8fa5db50ba274860f7a
sudipto <cg.sudipc@gmail.com>
2023-10-06 18:26:30 +0530

    ListAdapter impl - wip

---
+++ RE01-adapter-intent-wordsapp/README.md
@@ -0,0 +1,4 @@
+# Words App
+
+### Adapter, Intent implementation recap, by building the words app from scratch
+### reference : 2.3(affirmations app), 3.1(words app)
---
+++ RE01-adapter-intent-wordsapp/app/src/main/java/com/example/adapter_intent_recap/LetterAdapter.kt
@@ -0,0 +1,32 @@
+package com.example.adapter_intent_recap
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import androidx.recyclerview.widget.RecyclerView
+
+class LetterAdapter : RecyclerView.Adapter<LetterAdapter.LetterViewHolder>() {
+
+    // generating a char range from 'A' to 'Z' and converting to list
+    private val list = ('A').rangeTo('Z').toList()
+
+    class LetterViewHolder(val view: View) : RecyclerView.ViewHolder(view){
+        val button: Button = view.findViewById(R.id.button_item)
+
+    }
+
+    // this method is called by LayoutManager to create new view holders for RecyclerView
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): LetterViewHolder {
+        // inflate the list item layout
+        val layout = LayoutInflater.from(parent.context)
+            .inflate(R.layout.item_view, parent, false)
+        return LetterViewHolder(layout)
+    }
+
+    override fun onBindViewHolder(holder: LetterViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount() = list.size
+}
