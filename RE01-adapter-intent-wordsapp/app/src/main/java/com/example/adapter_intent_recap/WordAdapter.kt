e8e7d6e14e1a8ed0bcc8a7a022f7078c7d7605d4
sudipto <cg.sudipc@gmail.com>
2023-10-07 12:52:31 +0530

    added arrays.xml resource and WordAdapter class

--- RE01-adapter-intent-wordsapp/app/src/main/java/com/example/adapter_intent_recap/WordAdapter.kt
+++ RE01-adapter-intent-wordsapp/app/src/main/java/com/example/adapter_intent_recap/WordAdapter.kt
@@ -1,4 +1,40 @@
 package com.example.adapter_intent_recap
 
-class WordAdapter {
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import androidx.recyclerview.widget.RecyclerView
+
+class WordAdapter(private val letterId: String, context: Context) :
+    RecyclerView.Adapter<WordAdapter.WordViewHolder>() {
+
+    private val filteredWords: List<String>
+
+    init {
+        // Retrieve the list of words from res/values/arrays.xml
+        val words = context.resources.getStringArray(R.array.words).toList()
+        filteredWords = words
+            .filter { it.startsWith(letterId, ignoreCase = true) }
+            .shuffled()
+            .take(5)
+            .sorted()
+    }
+
+    class WordViewHolder(val view: View) : RecyclerView.ViewHolder(view) {
+        val button: Button = view.findViewById(R.id.button_item)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): WordViewHolder {
+        val layout = LayoutInflater.from(parent.context).inflate(R.layout.item_view, parent, false)
+        return WordViewHolder(layout)
+    }
+
+    override fun onBindViewHolder(holder: WordViewHolder, position: Int) {
+        val item = filteredWords.get(position)
+        holder.button.text = item.toString()
+    }
+
+    override fun getItemCount() = filteredWords.size
 }
