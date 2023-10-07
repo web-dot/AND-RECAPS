0ea62f02c9c4f0587833f45c3cb32c4e474cb35b
sudipto <cg.sudipc@gmail.com>
2023-10-07 13:19:20 +0530

    configured MainActivity and DetailActivity

--- RE01-adapter-intent-wordsapp/app/src/main/java/com/example/adapter_intent_recap/DetailActivity.kt
+++ RE01-adapter-intent-wordsapp/app/src/main/java/com/example/adapter_intent_recap/DetailActivity.kt
@@ -2,10 +2,31 @@ package com.example.adapter_intent_recap
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.adapter_intent_recap.databinding.ActivityDetailBinding
 
 class DetailActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_detail)
+        /**
+         * retrieving a binding object that allows to refer to view by id name
+         * Names are converted from snake case to camel case
+         * For example a view with the id word_one is referenced as binding.wordOne
+         * */
+        val binding = ActivityDetailBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val letterId = "A"
+
+        val recyclerView = binding.recyclerView
+        recyclerView.layoutManager = LinearLayoutManager(this)
+        recyclerView.adapter = WordAdapter(letterId, this)
+
+        recyclerView.addItemDecoration(
+            DividerItemDecoration(this, DividerItemDecoration.VERTICAL)
+        )
+
+        title = "Words That Start With" + " " + letterId
     }
 }
